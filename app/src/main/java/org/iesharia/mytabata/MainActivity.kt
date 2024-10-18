9559d6088cf27c530def08fea63e37b6db775821
iballa003 <iballa.montero@gmail.com>
2024-10-18 11:50:50 +0100

    Initial commit

---
+++ app/src/main/java/org/iesharia/mytabata/MainActivity.kt
@@ -0,0 +1,66 @@
+package org.iesharia.mytabata
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.iesharia.mytabata.ui.theme.MytabataTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MytabataTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Counter(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+    var theCounter by remember { mutableStateOf(0L) }
+    var miConterDown by remember{ mutableStateOf(CounterDown(99, {newvalue -> theCounter = newvalue}))}
+
+    Column {
+        Text(
+            text = theCounter.toString(),
+            modifier = modifier
+        )
+        Button(onClick = {
+            miConterDown.toggle()
+        }) {
+            Text(
+                text = "Pulsar"
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MytabataTheme {
+        Counter()
+    }
+}
